9be0780e9179fb87ecf45a4441a1907c11d8a50d
Milos Sundic <42153865+milossundic@users.noreply.github.com>
2019-11-08 13:36:16 +0100

    Font Awesome Added

---
+++ include/meta.php
@@ -0,0 +1,19 @@
+<meta charset="UTF-8">
+<meta name="google-site-verification" content="Q7P8qM_XSFxL5aBL7WN4AsxdqsI7CPrZcnGNY52Kr0Y"/>
+<title>EduTrak</title>
+
+<link href="view/css/reset.css" rel="stylesheet">
+<link href="view/css/style.css" rel="stylesheet">
+<link href="view/css/tagify.css" rel="stylesheet">
+<link href="view/css/font-awesome/css/all.min.css" rel="stylesheet">
+
+<link href="view/css/remodal.css" rel="stylesheet">
+<link href="view/css/remodal-default-theme.css" rel="stylesheet">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="view/js/jQuery.tagify.min.js"></script>
+<script src="view/js/tagify.js"></script>
+<script src="view/js/remodal.js"></script>
+
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/css/select2.min.css" rel="stylesheet"/>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/js/select2.min.js"></script>
--- index.php
+++ index.php
@@ -96,30 +96,7 @@ if($user->isLoggedIn()){
         <!DOCTYPE html>
         <html lang="en">
         <head>
-
-            <meta charset="UTF-8">
-            <meta name="google-site-verification" content="Q7P8qM_XSFxL5aBL7WN4AsxdqsI7CPrZcnGNY52Kr0Y"/>
-            <title>EduTrak</title>
-
-            <link href="view/css/reset.css" rel="stylesheet">
-            <link href="view/css/style.css" rel="stylesheet">
-            <link href="view/css/tagify.css" rel="stylesheet">
-
-            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
-                  integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
-                  crossorigin="anonymous">
-
-            <link href="view/css/remodal.css" rel="stylesheet">
-            <link href="view/css/remodal-default-theme.css" rel="stylesheet">
-
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-            <script src="view/js/jQuery.tagify.min.js"></script>
-            <script src="view/js/tagify.js"></script>
-            <script src="view/js/remodal.js"></script>
-
-            <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/css/select2.min.css" rel="stylesheet"/>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/js/select2.min.js"></script>
-
+            <?php include __DIR__ . '/include/meta.php'; ?>
         </head>
         <body>
 
